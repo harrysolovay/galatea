d5e1f526ff22e82e76ef0e76da3663387af67d48
Harry Solovay <harrysolovay@pm.me>
2024-10-23 22:20:57 -0400

    continued

--- Session.ts
+++ Session.ts
@@ -1,15 +1,34 @@
 import { Context } from "./Context.ts"
 import type { ClientEvent, ServerEvent } from "./events/mod.ts"
 import { handlers } from "./handlers.ts"
-import type { Content, ErrorDetails, SessionConfig as SessionConfig_, TurnDetection } from "./models/mod.ts"
-import type { Modality } from "./models/Modality.ts"
-import type { Voice } from "./models/Voice.ts"
-import { type RootTy, schema } from "./schema/mod.ts"
+import type { Content, ErrorDetails } from "./models/mod.ts"
+import { formatSessionConfigUpdate, type SessionConfig, type SessionUpdateConfig } from "./SessionConfig.ts"
 import { listen } from "./socket.ts"
 import { base64Encode } from "./util/arrayBufferToBase64.ts"
 import { generateId } from "./util/id.ts"
 
-export function Session(connect: () => WebSocket, options?: SessionConfig): Session {
+export interface Session {
+  /** Get a writable stream with which to append text to the input buffer. */
+  appendText(text: string): void
+  /** Get a writable stream with which to append audio to the input buffer. */
+  audioInput(): WritableStream<Int16Array>
+  /** Get a readable stream of PCM-encoded audio chunks. */
+  audio(): ReadableStream<Int16Array>
+  /** Get a readable stream of audio transcript tokens. */
+  transcript(includeInput?: boolean): ReadableStream<string>
+  /** Commit the current buffer and trigger a response (if turn detection enabled). */
+  commit(): void
+  /** Update the session configuration. */
+  update(sessionUpdateConfig: SessionUpdateConfig): void
+  /** Get a readable stream with which to observe errors. */
+  errors(): ReadableStream<ErrorDetails>
+  /** End the session. */
+  end(): void
+  /** See whether turn detection enabled. */
+  turnDetection(): boolean
+}
+
+export function Session(connect: () => WebSocket, config?: SessionConfig): Session {
   const ctl = new AbortController()
   const context = new Context(ctl.signal)
 
@@ -19,13 +38,10 @@ export function Session(connect: () => WebSocket, options?: SessionConfig): Sess
     ctl.signal,
   )
 
-  if (options) {
+  if (config) {
     send({
       type: "session.update",
-      session: {
-        ...sessionUpdate(options),
-        ...options.voice ? { voice: options.voice } : {},
-      },
+      session: formatSessionConfigUpdate(config),
     })
   }
 
@@ -101,7 +117,7 @@ export function Session(connect: () => WebSocket, options?: SessionConfig): Sess
   function update(config: SessionUpdateConfig) {
     send({
       type: "session.update",
-      session: sessionUpdate(config),
+      session: formatSessionConfigUpdate(config),
     })
   }
 
@@ -115,79 +131,5 @@ export function Session(connect: () => WebSocket, options?: SessionConfig): Sess
 
   function turnDetection() {
     return !context.sessionResource || !!context.sessionResource.turn_detection
-  }
-}
-
-export interface Session {
-  /** Get a writable stream with which to append text to the input buffer. */
-  appendText(text: string): void
-  /** Get a writable stream with which to append audio to the input buffer. */
-  audioInput(): WritableStream<Int16Array>
-  /** Get a readable stream of PCM-encoded audio chunks. */
-  audio(): ReadableStream<Int16Array>
-  /** Get a readable stream of audio transcript tokens. */
-  transcript(includeInput?: boolean): ReadableStream<string>
-  /** Commit the current buffer and trigger a response (if turn detection enabled). */
-  commit(): void
-  /** Update the session configuration. */
-  update(sessionUpdateConfig: SessionUpdateConfig): void
-  /** Get a readable stream with which to observe errors. */
-  errors(): ReadableStream<ErrorDetails>
-  /** End the session. */
-  end(): void
-  /** See whether turn detection enabled. */
-  turnDetection(): boolean
-}
-
-export interface SessionConfig extends SessionUpdateConfig {
-  /** The name of the desired voice. */
-  voice?: Voice
-}
-
-export interface SessionUpdateConfig {
-  /** Whether to enable the audio modality. */
-  mute?: boolean
-  /** Whether to receive input transcript events. */
-  inputTranscript?: boolean
-  /** Whether to enable turn detection. */
-  turnDetection?: false | Omit<TurnDetection, "type">
-  /** Tools to make accessible to the model. */
-  tools?: Record<string, Tool>
-}
-
-export function Tool<T extends RootTy>(
-  description: string,
-  type: T,
-  f: (instance: InstanceType<T>) => unknown,
-): Tool<T> {
-  return { description, type, f }
-}
-
-export interface Tool<T extends RootTy = any> {
-  /** What is the tool? */
-  description: string
-  /** Runtime representation of the structured output. */
-  type: T
-  /** The fn that receives the parameters and returns information related to the call. */
-  f: (args: InstanceType<T>) => unknown
-}
-
-function sessionUpdate(config: SessionUpdateConfig): SessionConfig_ {
-  return {
-    modalities: ["text", ...config.mute ? [] : ["audio"] satisfies Modality[]],
-    input_audio_transcription: config.inputTranscript ? { model: "whisper-1" } : null,
-    ...typeof config.turnDetection === "undefined" ? {} : config.turnDetection
-      ? { turn_detection: { type: "server_vad", ...config.turnDetection } }
-      : { turn_detection: null },
-    ...config.tools
-      ? {
-        tools: Object.entries(config.tools).map(([name, { description, type }]) => ({
-          type: "function",
-          name,
-          description,
-          parameters: schema({ type }, "type"),
-        })),
-      }
-      : {},
   }
 }
---
+++ SessionConfig.ts
@@ -0,0 +1,56 @@
+import type { Modality, SessionConfig as SessionConfig_, TurnDetection, Voice } from "./models/mod.ts"
+import { type RootTy, schema } from "./schema/mod.ts"
+
+export interface SessionConfig extends SessionUpdateConfig {
+  /** The name of the desired voice. */
+  voice?: Voice
+}
+
+export interface SessionUpdateConfig {
+  /** Whether to enable the audio modality. */
+  mute?: boolean
+  /** Whether to receive input transcript events. */
+  inputTranscript?: boolean
+  /** Whether to enable turn detection. */
+  turnDetection?: false | Omit<TurnDetection, "type">
+  /** Tools to make accessible to the model. */
+  tools?: Record<string, Tool>
+}
+
+export function Tool<T extends RootTy>(
+  description: string,
+  type: T,
+  f: (instance: InstanceType<T>) => unknown,
+): Tool<T> {
+  return { description, type, f }
+}
+
+export interface Tool<T extends RootTy = any> {
+  /** What is the tool? */
+  description: string
+  /** Runtime representation of the structured output. */
+  type: T
+  /** The fn that receives the parameters and returns information related to the call. */
+  f: (args: InstanceType<T>) => unknown
+}
+
+export function formatSessionConfigUpdate(config: SessionConfig): SessionConfig_ {
+  return {
+    modalities: ["text", ...config.mute ? [] : ["audio"] satisfies Modality[]],
+    input_audio_transcription: config.inputTranscript ? { model: "whisper-1" } : null,
+    ...typeof config.turnDetection === "undefined" ? {} : config.turnDetection
+      ? { turn_detection: { type: "server_vad", ...config.turnDetection } }
+      : { turn_detection: null },
+    ...config.tools
+      ? {
+        tools: Object.entries(config.tools).map(([name, { description, type }]) => ({
+          type: "function",
+          name,
+          description,
+          parameters: schema({ type }, "type"),
+        })),
+      }
+      : {},
+    ...config.voice ? { voice: config.voice } : {},
+  }
+}
