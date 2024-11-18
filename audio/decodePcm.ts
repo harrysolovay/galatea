f8c6f9a2b0708189624c6a69f319954c098e463a
Harry Solovay <harrysolovay@gmail.com>
2024-11-18 08:56:40 -0500

    continued

--- audio/decodePcm.ts
+++ audio/decodePcm.ts
@@ -1,14 +1,20 @@
-export function decodePcm(audioCtx: AudioContext, chunk: Int16Array): AudioBuffer {
+import { AudioContext } from "@mutefish/web-audio-api"
+
+export function decodePcm(chunk: Int16Array): AudioBuffer {
   const pcm = Array.from(chunk).map((i) => i / 32768)
-  const buf = audioCtx.createBuffer(1, pcm.length, 24_000)
+  const buf = AudioContext.prototype.createBuffer(1, pcm.length, 24_000)
   buf.getChannelData(0).set(pcm)
   return buf
 }
 
-export function decodePcmTransformer(audioCtx: AudioContext) {
-  return new TransformStream<Int16Array, AudioBuffer>({
-    transform(chunk, ctl) {
-      ctl.enqueue(decodePcm(audioCtx, chunk))
-    },
-  })
+// AudioContext.prototype.createBuffer
+
+export class PcmDecoderStream extends TransformStream<Int16Array, AudioBuffer> {
+  constructor() {
+    super({
+      transform(chunk, ctl) {
+        ctl.enqueue(decodePcm(chunk))
+      },
+    })
+  }
 }
--- examples/vad.ts
+++ examples/vad.ts
@@ -1,30 +1,20 @@
-import { decodeWav } from "audio"
+import { AudioContext, audioOutput, decodeWav, PcmDecoderStream } from "audio"
 import { conn, Session } from "galatea"
 import "@std/dotenv/load"
 import { fromFileUrl } from "@std/path"
 
 const session = new Session(() => conn(Deno.env.get("OPENAI_API_KEY")!))
 
+session.assistant
+  .audio()
+  .pipeThrough(new PcmDecoderStream())
+  .pipeTo(audioOutput(new AudioContext()))
+
+await audioInput().pipeTo(session.user.writeable())
+
+session.user.commit()
+
 function audioInput() {
   const bytes = Deno.readFileSync(fromFileUrl(import.meta.resolve("./tell_me.wav")))
   return ReadableStream.from([decodeWav(bytes as never).channelData[0]!])
 }
-
-{
-  ;(async () => {
-    for await (const token of session.assistant.text()) {
-      Deno.stdout.write(new TextEncoder().encode(token))
-    }
-  })()
-}
-
-audioInput().pipeTo(session.user.writeable())
-
-setTimeout(() => {
-  session.user.commit()
-}, 1000)
-
-setTimeout(() => {
-  session.user.write("Write a poem about it.")
-  session.assistant.respond()
-}, 10000)
